191a12bfb5491a969a112c27a65dbdeae7059176
Martin Hübner <martin.hubner@web.de>
2025-06-01 15:34:42 +0200

    feature: add berlin config

--- www/config.js
+++ www/config.js
@@ -5,13 +5,13 @@ var config = {
   show_help: true,
 
   // Versions list (optional if provided by .versions.json)
-  versions: ["23.05.4", "19.07.10"],
+  //versions: ["23.05.4", "19.07.10"],
 
   // Pre-selected version (optional if provided by .versions.json)
-  default_version: "23.05.4",
+  //default_version: "23.05.4",
 
   // Image download URL (e.g. "https://downloads.openwrt.org")
-  image_url: "../misc",
+  image_url: "https://firmware.berlin.freifunk.net",
 
   // Insert snapshot versions (optional)
   //show_snapshots: true,
@@ -20,6 +20,6 @@ var config = {
   info_url: "https://openwrt.org/start?do=search&id=toh&q={title} @toh",
 
   // Attended Sysupgrade Server support (optional)
-  asu_url: "https://sysupgrade.openwrt.org",
-  asu_extra_packages: ["luci"],
+  //asu_url: "https://sysupgrade.openwrt.org",
+  //asu_extra_packages: ["luci"],
 };
